8872dae874902adaf07e34c993c9a5eadfdef9fe
Danielle Marshall <ellemarshall5@gmail.com>
2021-01-02 16:10:39 -0700

    Add files via upload

---
+++ Mission01Marshall.java
@@ -0,0 +1,17 @@
+
+//this program will print my name and favorite movie
+
+
+public class Mission01Marshall
+
+{
+
+   public static void main(String[] args)
+   
+      {
+      
+         System.out.println("My name is Danielle Marshall."); //output to show my name
+         System.out.println("My favorite movie is Hereditary."); //output to show my favorite movie
+         
+      }
+}
